fc181aae8c32fbe085cf2a2714b61fb13309ecf7
Tito Wu <91169600+TitoWuPan@users.noreply.github.com>
2023-11-22 20:17:18 -0500

    Add files via upload

--- LudoPage/Script.js
+++ LudoPage/Script.js
@@ -2,12 +2,49 @@ const startButton = document.getElementById('startButton');
 const textContainer = document.querySelector('.text-container');
 const mazeContainer = document.getElementById('mazeContainer');
 
+const matrix = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 3, 0, 0, 0, 2, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 0, 0, 1, 0, 2, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 4, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+let valor = 1;
+
+const matrix_aux = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 3, 0, 0, 0, 2, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 0, 0, 1, 0, 2, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 4, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+
 startButton.addEventListener('click', function () {
     const newTask = document.createElement('p');
     
     newTask.innerHTML = '<span style="color: #4caf50;"></span> Nueva tarea agregada';
 
+    MazeTable()
+
+    // insertFicha(1, valor);
+
     textContainer.appendChild(newTask);
+    
     textContainer.scrollTop = textContainer.scrollHeight;
 });
 
@@ -16,45 +53,86 @@ function adjustTextContainerHeight() {
     document.getElementById('dynamicTextContainer').style.maxHeight = windowHeight + 'px';
 }
 
-function createMazeTable() {
-    const matrix = [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 3, 0, 0, 0, 2, 0, 0, 0, 0, 0, 1],
-        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
-        [1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 1, 0, 0, 0, 1, 0, 2, 0, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
-        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 4, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    ];
+function insertFicha(row, col) {
 
-    const table = document.createElement('table');
+    if (row >= 0 && row < matrix_aux.length && col >= 0 && col < matrix_aux[row].length) {
+        matrix[row][col] = 5; 
 
-    for (let i = 0; i < matrix.length; i++) {
-        const row = document.createElement('tr');
-
-        for (let j = 0; j < matrix[i].length; j++) {
-            const cell = document.createElement('td');
-
-            if (matrix[i][j] === 1) {
-                cell.className = 'wall';
-            } else if (matrix[i][j] === 2) {
-                cell.className = 'trap';
-            } else if (matrix[i][j] === 3) {
-                cell.className = 'start';
-            } else if (matrix[i][j] === 4) {
-                cell.className = 'end';
-            } else {
-                cell.className = 'empty';
-            }
-            row.appendChild(cell);
-        }
-        table.appendChild(row);
+        const targetCell = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
+        if (targetCell) {    targetCell.className = 'ficha';  }
+    } else {
+        console.error('Posición fuera de los límites de la matriz.');
     }
+}
+
+function MazeTable() {
+
+    mazeContainer.innerHTML = '';
+    
+    const table = document.createElement('mazeTable');
+
+    matrix_aux.forEach((row, rowIndex) => {
+        const tr = document.createElement('tr');
+        row.forEach((cellValue, colIndex) => {
+            const td = document.createElement('td');
+            td.setAttribute('data-row', rowIndex); 
+            td.setAttribute('data-col', colIndex);
+
+            if (cellValue === 1) {
+                td.className = 'wall';
+            } else if (cellValue === 2) {
+                td.className = 'trap';
+            } else if (cellValue === 3) {
+                td.className = 'start';
+            } else if (cellValue === 4) {
+                td.className = 'end';
+            } else if (cellValue === 5) {
+                td.className = 'ficha';
+            } else if (cellValue === 0) {
+                td.className = 'empty';
+            }
+
+            tr.appendChild(td);
+        });
+        table.appendChild(tr);
+    });
+
+    mazeContainer.appendChild(table);
+}
+
+
+function initMazeTable() {
+
+    mazeContainer.innerHTML = '';
+    
+    const table = document.createElement('mazeTable');
+
+    matrix.forEach((row, rowIndex) => {
+        const tr = document.createElement('tr');
+        row.forEach((cellValue, colIndex) => {
+            const td = document.createElement('td');
+            td.setAttribute('data-row', rowIndex); 
+            td.setAttribute('data-col', colIndex);
+
+            if (cellValue === 1) {
+                td.className = 'wall';
+            } else if (cellValue === 2) {
+                td.className = 'trap';
+            } else if (cellValue === 3) {
+                td.className = 'start';
+            } else if (cellValue === 4) {
+                td.className = 'end';
+            } else if (cellValue === 5) {
+                td.className = 'ficha';
+            } else if (cellValue === 0) {
+                td.className = 'empty';
+            }
+
+            tr.appendChild(td);
+        });
+        table.appendChild(tr);
+    });
+
     mazeContainer.appendChild(table);
 }
 
